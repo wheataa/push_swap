90c7976d9a2b35c9754d5b4d9bc8e5c0522f721b
Jack Wheating <jwheatin@c1r6s3.42london.com>
2026-01-07 17:16:58 +0000

    added len checks on functions, changed push

--- stackops1.c
+++ stackops1.c
@@ -6,41 +6,53 @@
 /*   By: jwheatin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 16:59:31 by jwheatin          #+#    #+#             */
-/*   Updated: 2026/01/07 16:28:30 by jwheatin         ###   ########.fr       */
+/*   Updated: 2026/01/07 17:12:44 by jwheatin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	sa(int **a)
+void	sa(int **a, int len_a)
 {
 	int	temp;
 
+	if (len_a <= 1)
+		return ;
 	temp = *a[0];
 	(*a)[0] = (*a)[1];
 	(*a)[1] = temp;
 
 }
 
-void	ss(int **a, int **b)
+void	ss(int **a, int **b, int len_a, int len_b)
 {
-	sa(a);
-	sa(b);
+	sa(a, len_a);
+	sa(b, len_b);
 }
 
-void	pa(int **a, int **b, int len)
+void	pa(int **a, int **b, int *len_a, int *len_b)
 {
 	int	i;
 	
+	if (!*len_b)
+		return ;
 	i = 0;
-	while (i < len - 1)
+	while (i < *len_a)
 	{
-		(*a)[i + 1] = (*a)[i];
+		(*a)[*len_a - i] = (*a)[*len_a - i - 1];
 		i++;
 	}
 	(*a)[0] = (*b)[0];
-	(*b)[0] = 0;
+	i = 0;
+	while (i < *len_b - 1)
+	{
+		(*b)[i] = (*b)[i + 1];
+		i++;
+	}
+	(*b)[i] = 0;
+	*len_a++;
+	*len_b--;
 		
 }
 
@@ -50,15 +62,27 @@ int	main(void)
 	int	*b;
 	int	i = 0;
 	int	j = 0;
+	int	len_a = 10;
+	int	len_b = 0;
 
-	a = malloc(5 * sizeof(int));
-	b = malloc(5 * sizeof(int));
+	a = malloc(10 * sizeof(int));
+	b = malloc(10 * sizeof(int));
 	while (i < 5)
 	{
 		a[i] = i + 1;
 		i++;
 	}
-	while (j < 5)
+	while (i < 10)
+	{
+		a[i] = 0;
+		i++;
+	}
+	/*while (j < 5)
+	{
+		b[j] = j + 6;
+		j++;
+	}*/
+	while (j < 10)
 	{
 		b[j] = 0;
 		j++;
@@ -67,15 +91,15 @@ int	main(void)
 	i = 0;
 	j = 0;
 	//ss(&a, &b);
-	pa(&b, &a);
+	pa(&a, &b, &len_a, &len_b);
 	printf("a is:\n");
-	while (i < 5)
+	while (i < 10)
 	{
 		printf("%i\n", a[i]);
 		i++;
 	}
 	printf("b is:\n");
-	while (j < 5)
+	while (j < 10)
 	{
 		printf("%i\n", b[j]);
 		j++;
--- stackops2.c
+++ stackops2.c
@@ -6,21 +6,23 @@
 /*   By: jwheatin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 16:59:31 by jwheatin          #+#    #+#             */
-/*   Updated: 2026/01/07 15:43:33 by jwheatin         ###   ########.fr       */
+/*   Updated: 2026/01/07 17:16:35 by jwheatin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	ra(int	**a, int len)
+void	ra(int	**a, int *len_a)
 {
 	int	temp;
 	int	i;
 
+	if (*len_a <= 1)
+		return ;
 	i = 0;
 	temp = (*a)[0];
-	while (i < len - 1)
+	while (i < *len_a - 1)
 	{
 		(*a)[i] = (*a)[i + 1];
 		i++;
@@ -28,31 +30,33 @@ void	ra(int	**a, int len)
 	(*a)[i] = temp;
 }
 
-void	rr(int	**a, int **b, int len)
+void	rr(int	**a, int **b, int *len_a, int *len_b)
 {
-	ra(a, len);
-	ra(b, len);
+	ra(a, len_a);
+	ra(b, len_b);
 }
 
-void	rra(int **a, int len)
+void	rra(int **a, int *len_a)
 {
 	int	temp;
 	int	i;
 
+	if (*len_a <= 1)
+		return ;
 	i = 0;
-	temp = (*a)[len - 1];
-	while (i < len - 1)
+	temp = (*a)[*len_a - 1];
+	while (i < *len_a - 1)
 	{
-		(*a)[len - i - 1] = (*a)[len - i - 2];
+		(*a)[*len_a - i - 1] = (*a)[*len_a - i - 2];
 		i++;
 	}
 	(*a)[0] = temp;
 }
 
-void	rrr(int **a, int **b, int len)
+void	rrr(int **a, int **b, int *len_a, int *len_b)
 {
-	rra(a, len);
-	rra(b, len);
+	rra(a, len_a);
+	rra(b, len_b);
 }
 
 int	main(void)
@@ -61,12 +65,19 @@ int	main(void)
 	int	*b;
 	int	i = 0;
 	int	j = 0;
+	int	len_a = 5;
+	int	len_b = 5;
 
-	a = malloc(5 * sizeof(int));
-	b = malloc(5 * sizeof(int));
+	a = malloc(10 * sizeof(int));
+	b = malloc(10 * sizeof(int));
 	while (i < 5)
 	{
 		a[i] = i + 1;
+		i++;
+	}
+	while (i < 10)
+	{
+		a[i] = 0;
 		i++;
 	}
 	while (j < 5)
@@ -74,19 +85,23 @@ int	main(void)
 		b[j] = j + 5;
 		j++;
 	}
-	
+	while (j < 10)
+	{
+		b[j] = 0;
+		j++;
+	}	
 	i = 0;
 	j = 0;
 	//ss(&a, &b);
-	rrr(&a, &b, 5);
+	rr(&a, &b, &len_a, &len_b);
 	printf("a is:\n");
-	while (i < 5)
+	while (i < 10)
 	{
 		printf("%i\n", a[i]);
 		i++;
 	}
 	printf("\nb is:\n");
-	while (j < 5)
+	while (j < 10)
 	{
 		printf("%i\n", b[j]);
 		j++;
